8c995e66b5a7dae1cb1d6e50da70dc3bc0181f47
Piotr Wasilewski <piotr.m.wasilewski@gmail.com>
2013-01-17 23:42:37 +0100

    added actual converting code :)

--- lib/bytes_converter.rb
+++ lib/bytes_converter.rb
@@ -1,5 +1,21 @@
 require "bytes_converter/version"
 
 module BytesConverter
-  # Your code goes here...
+
+  # method converting size into bytes
+  def self.convert size
+    sizes = {
+      "K" => 1024,
+      "M" => 1024*1024,
+      "G" => 1024*1024*1024
+    }
+    m = size.scan(/(\d*\.\d*)+(\w)?/).collect { |serwer| serwer }
+
+    unless m[0].nil?
+      out = m[0][0].to_f * sizes[m[0][1]].to_f
+    else
+      out = size
+    end
+    out
+  end
 end
